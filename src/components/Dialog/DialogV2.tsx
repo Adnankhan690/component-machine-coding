dd1e912187880399b99569bd7ecbb4ef3ca405ab
Adnan <adnan73803@gmail.com>
2026-08-01 15:58:47 +0530

    use react-portal for  dialog, handle multiple modal in root-modal

--- src/components/Dialog/DialogV2.tsx
+++ src/components/Dialog/DialogV2.tsx
@@ -1,26 +1,44 @@
 import { createPortal } from "react-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export default function DialogV2() {
-    // useEffect(() => {
-    //     const modalRoot = document.createElement("div");
-    //     modalRoot.id = "modal-root";
+interface DialogV2 {
+    show: boolean;
+    onClose: () => void;
+    onSubmit?: () => void;
+    children: React.ReactNode;
+}
 
-    //     document.body.appendChild(modalRoot);
-
-    // }, [])
+export default function DialogV2({
+    show = true,
+    onClose,
+    onSubmit,
+    children
+}: DialogV2) {
+    const [container, setContainer] = useState<HTMLDivElement | null>(null);
 
     useEffect(() => {
-        const modalRoot = document.createElement("div");
-        modalRoot.id = "modal-root";
-        document.body.appendChild(modalRoot);
+        let created = false;
+
+        let modalRoot = document.getElementById("modal-root") as HTMLDivElement;
+
+        if (!modalRoot) {
+            modalRoot = document.createElement("div");
+            modalRoot.id = "modal-root";
+
+            document.body.appendChild(modalRoot);
+            created = true;
+        }
+
+        setContainer(modalRoot);
 
         return () => {
-            if (modalRoot && modalRoot.parentNode) {
-                modalRoot.parentNode.removeChild(modalRoot);
+            if (modalRoot && modalRoot.childNodes.length === 0) {
+                modalRoot.remove();
             }
-        };
-    }, []);
+        }
+    }, [])
+
+    if (!container) return null;
 
     return createPortal(
         <>
