0f3c5b4c0e116b60df12db9d63a8d79af4018312
Adnan <adnan73803@gmail.com>
2026-08-02 12:14:32 +0530

    add accordian future workscope

--- src/screens/accordian/ScreenAccordian.tsx
+++ src/screens/accordian/ScreenAccordian.tsx
@@ -1,3 +1,5 @@
+// todo: use adapter pattern for decoupling data-type
+// todo: use compound pattern so that style of the accordian can be customised.
 import AccordianV2 from "./components/AccordianV2";
 import AccordianV3 from "./components/AccordianV3";
 import { accordianData } from "./hooks/useAccordian";
