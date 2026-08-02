8ab4d1d97425b9811f78b2e63bd3e8053d59de1a
Adnan <adnan73803@gmail.com>
2026-08-02 12:12:27 +0530

    add slider transition for the accordian

--- src/screens/accordian/components/AccordianV3.tsx
+++ src/screens/accordian/components/AccordianV3.tsx
@@ -1,3 +1,4 @@
+import "../accordianV3.css";
 import useAccordianV3 from "../hooks/useAccordianV3";
 
 export interface Accordian {
@@ -13,17 +14,25 @@ interface AccordianV3Prop {
 export default function AccordianV3({ data }: AccordianV3Prop) {
     const { accordianId, toggleAccordian } = useAccordianV3({ data });
 
+    const isOpen = (id: string) => {
+        return accordianId.includes(id);
+    }
+
     return (
-        <div>
+        <div className="accordian-con">
             {data.map(({ title, description, id }, idx) => (
-                <div key={id}>
+                <div key={id} className="accordian-box-wrapper">
                     {/* // title and btn  */}
-                    <div>
+                    <div className="title-btn-wrapper">
                         <p>{title}</p>
                         <button onClick={() => toggleAccordian(id)}>{accordianId === id ? '-' : '+'}</button>
                     </div>
                     {/* content  */}
-                    <div></div>
+                    <div className={`desc-con ${isOpen(id) ? 'open' : ''}`}>
+                        <div className="desc-height">
+                            <div>{description}</div>
+                        </div>
+                    </div>
                 </div>
             ))}
         </div>
