e3c22d4dff1683c6062c0599c017cf6a4df04dab
Adnan <adnan73803@gmail.com>
2026-08-07 23:43:27 +0530

    add stop-watch controls

--- src/screens/StopWatch/componentsV3/StopWatchV3.tsx
+++ src/screens/StopWatch/componentsV3/StopWatchV3.tsx
@@ -2,6 +2,12 @@ export default function StopWatchV3(){
     return(
         <div>
             <p>Stop watch v3</p>
+            <p></p>
+            <div className="stop-watch-controls">
+                <button>start</button>
+                <button>pause</button>
+                <button>reset</button>
+            </div>
         </div>
     )
 }
