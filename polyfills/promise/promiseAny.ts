43f8036afe8a39b16fa93efe22bad5ca2a9e384d
Adnan <adnan73803@gmail.com>
2026-09-07 22:37:41 +0530

    add promise.any polyfill

---
+++ polyfills/promise/promiseAny.ts
@@ -0,0 +1,38 @@
+function myPromiseAny(promises) {
+	return new Promise((resolve, reject) => {
+		// const promiseArray =
+		if (promises.length === 0) {
+			return reject(new AggregateError([], "All promises were rejected"));
+		}
+
+		const errors = new Array(promises.length);
+		let errorCount = 0;
+
+		promises.forEach((promise, idx) => {
+			Promise.resolve(promise)
+				.then((ele) => {
+					resolve(ele);
+				})
+				.catch((error) => {
+					errorCount++;
+					errors[idx] = error;
+
+					if (errorCount === promises.length) {
+						reject(new AggregateError([], "All promises were rejected"));
+					}
+				});
+		});
+	});
+}
+
+const promiseOneReject = Promise.reject("Rjected promise 1");
+const promiseTwoSucc1 = Promise.reject("Resolved 1");
+const promiseTwoSucc2 = Promise.reject("Resolved 2");
+
+myPromiseAny([promiseOneReject, promiseTwoSucc1, promiseTwoSucc2])
+	.then((res) => {
+		console.log(res);
+	})
+	.catch((error) => {
+		console.log(error);
+	});
