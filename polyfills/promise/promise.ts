f4b7f5ae54f08092c2cc8bd5caf04ef1ff443b01
Adnan <adnan73803@gmail.com>
2026-09-11 00:08:44 +0530

    add class constructor for promise

--- polyfills/promise/promise.ts
+++ polyfills/promise/promise.ts
@@ -1,7 +1,33 @@
-
+const FULFILLED = 'FULFILLED';
+const PENDING = 'PENDING';
+const REJECTED = 'REJECTED';
 
 class MyPromise {
     constructor(context) {
+        this.value = null;
+        this.reason = null;
+        this.status = PENDING;
+    }
 
+    const resolve = (value) => {
+        if (this.status === PENDING) {
+            this.value = value;
+            this.status = FULFILLED;
+        }
+    }
+
+    const rejected = (reason) => {
+        if (this.status === PENDING) {
+            this.reason = reason;
+            this.status = REJECTED;
+        }
+    }
+
+    try {
+        context(resolve, rejected);
+    } catch(error) {
+        rejected(error);
     }
 }
+
+then
