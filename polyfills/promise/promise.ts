7aef5ef0565d46a61207130db30d2f5578e3f70e
Adnan <adnan73803@gmail.com>
2026-09-11 00:26:11 +0530

    add basic promise polyfill

--- polyfills/promise/promise.ts
+++ polyfills/promise/promise.ts
@@ -1,33 +1,67 @@
-const FULFILLED = 'FULFILLED';
-const PENDING = 'PENDING';
-const REJECTED = 'REJECTED';
+const FULFILLED = "FULFILLED";
+const PENDING = "PENDING";
+const REJECTED = "REJECTED";
 
 class MyPromise {
-    constructor(context) {
-        this.value = null;
-        this.reason = null;
-        this.status = PENDING;
-    }
+	constructor(context) {
+		this.value = null;
+		this.reason = null;
+		this.status = PENDING;
+		this.onFulfilled = [];
+		this.onRejected = [];
 
-    const resolve = (value) => {
-        if (this.status === PENDING) {
-            this.value = value;
-            this.status = FULFILLED;
-        }
-    }
+		const resolve = (value) => {
+			if (this.status === PENDING) {
+				this.value = value;
+				this.status = FULFILLED;
+				this.onFullfilled.forEach((cb) => cb(this.value));
+			}
+		};
 
-    const rejected = (reason) => {
-        if (this.status === PENDING) {
-            this.reason = reason;
-            this.status = REJECTED;
-        }
-    }
+		const rejected = (reason) => {
+			if (this.status === PENDING) {
+				this.reason = reason;
+				this.status = REJECTED;
+				this.onRejected.forEach((cb) => cb(this.reason));
+			}
+		};
 
-    try {
-        context(resolve, rejected);
-    } catch(error) {
-        rejected(error);
-    }
+		try {
+			context(resolve, rejected);
+		} catch (error) {
+			rejected(error);
+		}
+	}
+
+	then(onFullfilled, onRejected) {
+		if (this.status === FULFILLED) {
+			try {
+				onFullfilled(this.value);
+			} catch (error) {}
+		}
+
+		if (this.status === REJECTED) {
+			try {
+				onRejected(this.reason);
+			} catch (error) {}
+		}
+
+		if (this.status === PENDING) {
+			try {
+				this.onFullfilled.push(onFullfilled);
+			} catch (error) {}
+
+			this.onFullfilled.push((value) => {
+				try {
+					onFullfilled(value);
+				} catch (error) {}
+			});
+
+			this.onRejected.push((reason) => {
+				try {
+					onRejected(reason);
+				} catch (error) {}
+			});
+		}
+	}
 }
-
-then
