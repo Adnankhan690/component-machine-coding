693747dc310b6f913b741d276d4e480f2353943e
Adnan <adnan73803@gmail.com>
2026-09-15 00:29:14 +0530

    add solution for callNTimes API when failed

---
+++ polyfills/promise/apiCallNtimes.ts
@@ -0,0 +1,28 @@
+async function retryApiNTimes(n: number) {
+	if (n === maxAttempts) return;
+	try {
+		const response = await fetch(
+			"https://jsonplaceholder.typicode.com/todos/10000",
+		);
+
+		if (!response.ok) {
+			retryApiNTimes(n + 1);
+			throw new Error("failed to fetch api");
+		}
+
+		const data = await response.json();
+		console.log(data, "res");
+	} catch (error) {
+		console.log(error, "EError");
+	}
+}
+
+const maxAttempts = 5;
+
+function callNTimes() {
+	let count = 0;
+
+	retryApiNTimes(count);
+}
+
+callNTimes();
