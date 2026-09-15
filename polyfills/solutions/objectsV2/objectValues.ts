3ee029c6a03781dd8aac3f7c81c8ca22fc7246d5
Adnan <adnan73803@gmail.com>
2026-09-16 00:11:33 +0530

    add poylfill for objectValuesV2

---
+++ polyfills/solutions/objectsV2/objectValues.ts
@@ -0,0 +1,16 @@
+function myObjectValues(obj: unknown) {
+    if (obj === null || obj === undefined) {
+        throw new TypeError('Can not convert null or undfined to object');
+    }
+
+    const result = [];
+    const o = Object(obj);
+
+    for (let key in o) {
+        if (Object.prototype.hasOwnProperty.call(o, key)) {
+            result.push(o[key]);
+        }
+    }
+
+    return result;
+}
