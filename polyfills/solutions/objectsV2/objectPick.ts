d398181fbe52ef36025f72366d1dc6cfabf9ba58
Adnan <adnan73803@gmail.com>
2026-09-23 00:52:44 +0530

    add test cases for the pick

--- polyfills/solutions/objectsV2/objectPick.ts
+++ polyfills/solutions/objectsV2/objectPick.ts
@@ -1,18 +1,45 @@
-function myPick(obj) {
-    if (obj === null || obj === undefined) {
-        throw new TypeError("can not convert null or undefined to obj");
-    }
+function myPick(source, keys) {
+	if (source === null || source === undefined) {
+		throw new TypeError("can not convert null or undefined to obj");
+	}
 
-    const result = [];
-    let O = Object(obj);
+	// if (typeof source[Symbol.iterator] !== "function") {
+	// 	throw new TypeError(`${source} does not have an iterator`);
+	// }
 
-    // if()
+	const result = {};
+	let O = Object(source);
 
-    for (let key in O) {
-        if (Object.prototype.hasOwnProperty.call(O, key)) {
-            result[key] = O[key];
-        }
-    }
+	for (let key of keys) {
+		if (Object.prototype.hasOwnProperty.call(O, key)) {
+			result[key] = O[key];
+		}
+	}
 
-    return O;
+	return result;
 }
+
+Object.myPick = myPick;
+
+const sourceObj = { a: 1, b: 2, c: 3 };
+
+console.log(
+    Object.myPick(sourceObj, ['a', 'b'])
+)
+
+const set = new Set(['a', 'c']);
+console.log(Object.myPick(sourceObj, set));
+
+const parentObject = { inheritedInfo: "dna" };
+const childObject = Object.create(parentObject);
+childObject.name = "adnan";
+
+console.log(
+    Object.myPick(childObject, ['inheritedInfo', 'name'])
+)
+
+const symKey = Symbol("name");
+const secrectObj = { [symKey]: "ssh", hidden: "yes" };
+console.log(
+    Object.myPick(secrectObj, [symKey])
+)
